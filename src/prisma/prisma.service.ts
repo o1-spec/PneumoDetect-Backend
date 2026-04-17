a90a3e40aaf18efb881603856a742af030100929
o1-spec <oluwafemionadokun@gmail.com>
2026-04-17 03:25:06 +0100

    added fix to the prisma section

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,12 +2,43 @@ import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService implements OnModuleInit, OnModuleDestroy {
+  private prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
   async onModuleInit() {
-    await this.$connect();
+    await this.prisma.$connect();
   }
 
   async onModuleDestroy() {
-    await this.$disconnect();
+    await this.prisma.$disconnect();
+  }
+
+  // Proxy methods
+  get $connect() {
+    return this.prisma.$connect.bind(this.prisma);
+  }
+
+  get $disconnect() {
+    return this.prisma.$disconnect.bind(this.prisma);
+  }
+
+  get user() {
+    return this.prisma.user;
+  }
+
+  get patient() {
+    return this.prisma.patient;
+  }
+
+  get scan() {
+    return this.prisma.scan;
+  }
+
+  get notification() {
+    return this.prisma.notification;
   }
 }
